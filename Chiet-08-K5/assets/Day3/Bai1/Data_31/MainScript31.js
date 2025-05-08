a1500ef3c84a242fe2d9fb75dd3df8a2d8559832
Triet Nguyen <trietnguyenhoangminh@gmail.com>
2025-05-08 23:23:26 +0700

    refactor code and done basic function day3_bai1

--- Chiet-08-K5/assets/Day3/Bai1/Data_31/MainScript31.js
+++ Chiet-08-K5/assets/Day3/Bai1/Data_31/MainScript31.js
@@ -1,3 +1,6 @@
+const Data = require('Data');
+const GameController = require('GameController'); 
+
 cc.Class({
     extends: cc.Component,
 
@@ -14,94 +17,17 @@ cc.Class({
     },
 
     onLoad() {
-        this.inventory = [
-            { name: "Bình máu", quantity: 3, type: "consumable", effect: "Hồi 20 máu" },
-            { name: "Kiếm Sắt", quantity: 1, type: "equipment", effect: "Tăng 10 sức mạnh" },
-            { name: "Bình năng lượng", quantity: 2, type: "consumable", effect: "Hồi 15 năng lượng" },
-            { name: "Giáp Sắt", quantity: 1, type: "equipment", effect: "Tăng 20 giáp" },
-            { name: "Bình máu lớn", quantity: 1, type: "consumable", effect: "Hồi 50 máu" },
-            { name: "Kiếm Lửa", quantity: 1, type: "equipment", effect: "Tăng 25 sức mạnh và gây sát thương lửa" },
-            { name: "Khiên Gỗ", quantity: 1, type: "equipment", effect: "Tăng 10 giáp" },
-            { name: "Bình độc", quantity: 2, type: "consumable", effect: "Gây 30 sát thương cho kẻ địch" },
-            { name: "Vòng cổ hồi phục", quantity: 1, type: "equipment", effect: "Hồi 5 máu mỗi lượt" },
-            { name: "Thuốc tàng hình", quantity: 1, type: "consumable", effect: "Tàng hình trong 10 giây" }
-        ];
-        
-        this.selectedItem = null;
-        this.selectedIndex = null;
-        this.showItem();
-    },
-
-    showItem() {
-        this.itemListNode.removeAllChildren();
-        this.inventory.forEach((item, index) => {
-            const itemNode = cc.instantiate(this.itemPrefab);
-            itemNode.getComponentInChildren(cc.Label).string = item.name;
-            itemNode.on('click', () => this.selectItem(index), this);
-            this.itemListNode.addChild(itemNode);
-        });
-        this.clearInfo();
-    },
-
-    selectItem(index) {
-        this.selectedIndex = index;
-        const item = this.inventory[index];
-        this.selectedItem = item;
-
-        this.nameLabel.string = item.name;
-        this.quantityLabel.string = "Số lượng: " + item.quantity;
-        this.typeLabel.string = "Loại: " + item.type;
-        this.effectLabel.string = item.effect;
-
-        this.useButton.interactable = item.type === "consumable" && item.quantity > 0;
-        this.deleteButton.interactable = true;
-    },
-
-    clearInfo() {
-        this.nameLabel.string = "";
-        this.quantityLabel.string = "";
-        this.typeLabel.string = "";
-        this.effectLabel.string = "";
-        this.selectedItem = null;
-        this.selectedIndex = null;
-        this.useButton.interactable = false;
-        this.deleteButton.interactable = false;
+        const data = new Data();
+        this.gameController = new GameController(data, this);
+        this.gameController.showItem();
     },
 
     useItem() {
-        const item = this.selectedItem;
-        if (!item) return;
-
-        if (item.type === "consumable") {
-            item.quantity -= 1;
-            if (item.quantity <= 0) {
-                this.inventory.splice(this.selectedIndex, 1);
-            }
-        } else if (item.type === "equipment") {
-            this.showMessage("Đã trang bị " + item.name);
-            this.inventory.splice(this.selectedIndex, 1);
-        }
-
-        this.refreshUI();
+        this.gameController.useItem();
     },
 
     deleteItem() {
-        if (this.selectedIndex != null) {
-            const name = this.inventory[this.selectedIndex].name;
-            this.showMessage("Đã xóa " + name);
-            this.inventory.splice(this.selectedIndex, 1);
-            this.refreshUI();
-        }
-    },
-
-    refreshUI() {
-        this.showItem();
-
-        if (this.selectedIndex != null && this.selectedIndex < this.inventory.length) {
-            this.selectItem(this.selectedIndex);
-        } else {
-            this.clearInfo();
-        }
+        this.gameController.deleteItem();
     },
 
     showMessage(msg) {
